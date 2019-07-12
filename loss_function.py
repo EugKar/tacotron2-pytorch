Fixing numerical error. Replacing prior log variance with standard deviation. Tuning std initial values.

loss_function.py
@@ -27,7 +27,7 @@ class VAELoss(nn.Module):
         super(VAELoss, self).__init__()
         self.tacotron2loss = Tacotron2Loss()
 
-    def get_q_y_x(self, z_mu, z_logvar, z_prior_mu, z_prior_logvar):
+    def get_q_y_x(self, z_mu, z_logvar, z_prior_mu, z_prior_sigma):
         '''
         Calculate q(y_l|X) using Monte-Carlo sampling (1 sample)
         Input dimensions:
@@ -42,9 +42,9 @@ class VAELoss(nn.Module):
         k = z_prior_mu.size()[0]
         z_sample = q_z_x.rsample().unsqueeze(1).repeat([1, k, 1]) # B x K x D
         p_z_y = MultivariateNormal(z_prior_mu,
-            covariance_matrix=z_prior_logvar.exp().diag_embed())
+            scale_tril=z_prior_sigma.diag_embed())
         y_probs = p_z_y.log_prob(z_sample).exp()
-        q_y_x = y_probs / y_probs.sum(dim=1, keepdim=True).add_(EPS)
+        q_y_x = y_probs / y_probs.sum(dim=1, keepdim=True) #.add_(EPS)
         return q_y_x
 
     def kl_multivar_norm_diag(self, mu0, logvar0, mu1, logvar1):
@@ -75,24 +75,24 @@ class VAELoss(nn.Module):
         # Parameters of q(z_o | X), dimensions: B x D_o
         observed_mu, observed_logvar = observed_params
         # Parameters of p(z_l | y_l), dimensions: K_l x D_l
-        latent_prior_mu, latent_prior_logvar = latent_prior_params
+        latent_prior_mu, latent_prior_sigma = latent_prior_params
         # Parameters of p(z_o | y_o), dimensions: K_o x D_o
-        observed_prior_mu, observed_prior_logvar = observed_prior_params
+        observed_prior_mu, observed_prior_sigma = observed_prior_params
 
         # q(y_l | X), dimensions: B x K_l
         q_yl_x = self.get_q_y_x(latent_mu, latent_logvar, latent_prior_mu,
-                               latent_prior_logvar)
+                               latent_prior_sigma)
 
         # p(z_o | y_o) parameters, dimensions: B x D_o
         d_o = observed_mu.size()[1]
         yo_inds = y_observed.unsqueeze(1).repeat([1, d_o])
         observed_prior_mu_yo = observed_prior_mu.gather(0, yo_inds)
-        observed_prior_logvar_yo = observed_prior_logvar.gather(0, yo_inds)
+        observed_prior_sigma_yo = observed_prior_sigma.gather(0, yo_inds)
 
         # D_KL(q(z_o | X) || p(z_o || y_o)), dimensions: B
         kl_z_observed = self.kl_multivar_norm_diag(
             observed_mu, observed_logvar,
-            observed_prior_mu_yo, observed_prior_logvar_yo)
+            observed_prior_mu_yo, observed_prior_sigma_yo.pow(2).log())
 
         # D_KL(q(z_l | X) || p(z_l || y_l)), dimensions: B x K_l
         k_l = latent_prior_mu.size()[0]
@@ -101,7 +101,7 @@ class VAELoss(nn.Module):
             latent_mu.unsqueeze(1).repeat([1, k_l, 1]),
             latent_logvar.unsqueeze(1).repeat([1, k_l, 1]),
             latent_prior_mu.unsqueeze(0).repeat([b, 1, 1]),
-            latent_prior_logvar.unsqueeze(0).repeat([b, 1, 1]))
+            latent_prior_sigma.unsqueeze(0).repeat([b, 1, 1]).pow(2).log())
 
         # D_KL(q(y_l | X) || p(y_l)), dimensions: B
         kl_y_latent = (q_yl_x * (q_yl_x.add_(EPS).log() + math.log(k_l))).sum(dim=1)

model.py
@@ -612,10 +612,10 @@ class VAE(nn.Module):
         super(VAE, self).__init__()
         y_dim, z_dim = hparams.latent_y_output_dim, hparams.latent_z_output_dim
         self.latent_prior_mu = nn.Parameter(torch.zeros((y_dim, z_dim), requires_grad=True))
-        self.latent_prior_logvar = nn.Parameter(torch.ones((y_dim, z_dim), requires_grad=True) * hparams.latent_logvar_init)
+        self.latent_prior_sigma = nn.Parameter(torch.ones((y_dim, z_dim), requires_grad=True) * hparams.latent_sigma_init)
         y_dim, z_dim = hparams.observed_y_output_dim, hparams.observed_z_output_dim
         self.observed_prior_mu = nn.Parameter(torch.zeros((y_dim, z_dim), requires_grad=True))
-        self.observed_prior_logvar = nn.Parameter(torch.ones((y_dim, z_dim), requires_grad=True) * hparams.observed_logvar_init)
+        self.observed_prior_sigma = nn.Parameter(torch.ones((y_dim, z_dim), requires_grad=True) * hparams.observed_sigma_init)
 
         self.latent_z = LatentEncoder(hparams, hparams.latent_z_output_dim)
         self.observed_z = LatentEncoder(hparams, hparams.observed_z_output_dim)
@@ -649,8 +649,8 @@ class VAE(nn.Module):
         return (self.synthesizer(inputs, z_latent, z_observed),
             (latent_z_mu, latent_z_logvar),
             (observed_z_mu, observed_z_logvar),
-            (self.latent_prior_mu, self.latent_prior_logvar),
-            (self.observed_prior_mu, self.observed_prior_logvar))
+            (self.latent_prior_mu, self.latent_prior_sigma),
+            (self.observed_prior_mu, self.observed_prior_sigma))
 
     def inference(self, inputs, z_latent, z_observed):
         return self.synthesizer.inference(inputs, z_latent, z_observed)