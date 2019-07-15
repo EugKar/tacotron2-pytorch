713b7ec0dd3c775a6f139c21388db6d5e7c10df4
Eugene Karaulov <ekaraulov@sciforce.solutions>
2019-07-15 09:21:40 +0300

    Fixing nan checks.

--- loss_function.py
+++ loss_function.py
@@ -93,7 +93,7 @@ class VAELoss(nn.Module):
         # q(y_l | X), dimensions: B x K_l
         q_yl_x = self.q_y_x(latent_mu, latent_logvar, latent_prior_mu,
                                latent_prior_sigma)
-        if q_yl_x.isnan().any() or q_yl_x.isinf().any():
+        if torch.isnan(q_yl_x).any() or torch.isinf(q_yl_x).any():
             raise ValueError('q_y_x is inf or NaN')
 
         # p(z_o | y_o) parameters, dimensions: B x D_o
@@ -106,7 +106,7 @@ class VAELoss(nn.Module):
         kl_z_observed = self.kl_multivar_norm_diag(
             observed_mu, observed_logvar,
             observed_prior_mu_yo, observed_prior_sigma_yo.pow(2).log())
-        if kl_z_observed.isnan().any() or kl_z_observed.isinf().any():
+        if torch.isnan(kl_z_observed).any() or torch.isinf(kl_z_observed).any():
             raise ValueError('kl_z_observed is inf or NaN')
 
         # D_KL(q(z_l | X) || p(z_l || y_l)), dimensions: B x K_l
@@ -117,15 +117,15 @@ class VAELoss(nn.Module):
             latent_logvar.unsqueeze(1).repeat([1, k_l, 1]),
             latent_prior_mu.unsqueeze(0).repeat([b, 1, 1]),
             latent_prior_sigma.unsqueeze(0).repeat([b, 1, 1]).pow(2).log())
-        if kl_z_latent.isnan().any() or kl_z_latent.isinf().any():
+        if torch.isnan(kl_z_latent).any() or torch.isinf(kl_z_latent).any():
             raise ValueError('kl_z_latent is inf or NaN')
 
         # D_KL(q(y_l | X) || p(y_l)), dimensions: B
         kl_y_latent = (q_yl_x * (q_yl_x.add_(EPS).log() + math.log(k_l))).sum(dim=1)
-        if kl_y_latent.isnan().any() or kl_y_latent.isinf().any():
+        if torch.isnan(kl_y_latent).any() or torch.isinf(kl_y_latent).any():
             raise ValueError('kl_y_latent is inf or NaN')
 
         elbo = mel_loss + kl_z_observed.mean() + (q_yl_x * kl_z_latent).sum(dim=1).mean() + kl_y_latent.mean()
-        # if torch.isnan(elbo).any() or torch.isinf(elbo).any():
-        #     raise ValueError('ELBO is Inf or NaN')
+        if torch.isnan(elbo).any() or torch.isinf(elbo).any():
+            raise ValueError('ELBO is Inf or NaN')
         return elbo, mel_loss, gate_loss
