2fcabfca19ea50a09478842a08640e76ee7f1f60
Eugene Karaulov <ekaraulov@sciforce.solutions>
2019-08-17 23:20:15 +0300

    Adding param for simulating large batch.

--- hparams.py
+++ hparams.py
@@ -103,7 +103,8 @@ def create_hparams(hparams_string=None, verbose=False):
         weight_decay=1e-6,
         grad_clip_thresh=1.0,
         batch_size=64,
-        mask_padding=True  # set model's padded outputs to padded values
+        mask_padding=True,  # set model's padded outputs to padded values
+        smaller_batch_division=1
     )
 
     if hparams_string:
